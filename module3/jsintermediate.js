80ca42be7a8f29d86d4e5e4bd03d1c7af0bfd798
Tris-Dream <tj2001john@gmail.com>
2024-08-31 12:58:58 +1000

    lab

--- module3/jsintermediate.js
+++ module3/jsintermediate.js
@@ -24,21 +24,59 @@ function ucFirstLetters(str) {
 console.log(ucFirstLetters("los angeles")); //Los Angeles
 
 
-
 //Q2
 
+// function truncate(str, max) {
+//   if (str.length > max) {
+//     return str.slice(0, max) + "...";
+//   } else {
+//     return str;
+//   }
+//}
+
 function truncate(str, max) {
-  if (str.length > max) {
-    return str.slice(0, max) + "...";
-  } else {
-    return str;
-  }
+  const slice = str.length > max;
+  return str.slice(0, max) + "...";
 }
 
 console.log(truncate("This text will be truncated if it is too long", 25));
-*/
+
 function truncate(str, max) {
   return str.length > max ? str.slice(0, max) + "..." : str;
 }
 
 console.log(truncate("This text will be truncated if it is too long", 25));
+
+*/
+
+//Q3
+
+const animals = ["Tiger", "Giraffe"];
+console.log(animals);
+
+animals.push("Lion", "Bear");
+console.log(animals);
+
+animals.unshift("Cat", "Dog");
+console.log(animals);
+
+const sortedAnimals = [...animals].sort();
+
+console.log(sortedAnimals);
+
+function replaceMiddleAnimal(newValue) {
+  const animals2 = [...animals];
+  animals2.splice(2, 1, newValue);
+  console.log(animals2);
+}
+
+console.log(replaceMiddleAnimal("Bird"));
+
+function findMatchingAnimals(beginsWith) {
+  let animals3 = [...animals].filter((animals3) =>
+    animals3.startsWith(beginsWith)
+  );
+  return animals3;
+}
+
+console.log(findMatchingAnimals("B"));
